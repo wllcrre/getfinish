b6e68951f0a348d5961aae1ae8b028417e393f47
kevin <kevin@idea3cdeMacBook-Pro.local>
2017-09-18 17:44:23 +0800

    fix事項編輯頁的版面問題

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -6,49 +6,47 @@
 
 @section('content')
 
-	<div class="row">
-
 		{{-- {!! Form::model($item, array('route' => ['items.update', $item->id])) !!} --}}
 		<!--Important : Must add PUT Method for items.update-->
 		{!! Form::model($item, array('route' => ['items.update', $item->id],'method' => 'PUT')) !!}
 		
 
+		<div class="row">
 
+			<div class="col-md-8">
+				{{ Form::label('name','Name:') }}
+				{{ Form::text('name',null,["class" => 'form-control input-lg']) }}
 
-		<div class="col-md-8">
-			{{ Form::label('name','Name:') }}
-			{{ Form::text('name',null,["class" => 'form-control input-lg']) }}
-
-			{{ Form::label('body','Body:',['class' => 'form-spacing-top']) }}
-			{{ Form::textarea('body',null,["class" => 'form-control']) }}
-			 
-		</div>
-
-		<div class="col-md-4">
-			<div class="card card-body bg-light">
-				<dl class="">
-				  <dt>Create At:</dt>
-				  <dd>{{ date('Y-m-j h:i a', strtotime($item->created_at)) }}</dd>
-				</dl>
-
-				<dl class="">
-				  <dt>Last Updated:</dt>
-				  <dd>{{ date('Y-m-j h:i a', strtotime($item->updated_at)) }}</dd>
-				</dl>
-				<hr>
-
-				<div class="row">
-					<div class="col-sm-6">
-						{!! Html::linkRoute('items.show', 'Cancel', array($item->id), array('class' => 'btn btn-danger btn-block'))!!}
-						{{-- <a href="#" class='btn btn-primary btn-block'>Edit</a> --}}
-					</div>
-					<div class="col-sm-6">
-						{{-- {!! Html::linkRoute('items.update', 'Save', array($item->id), array('class' => 'btn btn-success btn-block'))!!} --}}
-						{{ Form::submit('Save Changes', array('class' => 'btn btn-success btn-block'))}}				
-					</div>					
-				</div>				
+				{{ Form::label('body','Body:',['class' => 'form-spacing-top']) }}
+				{{ Form::textarea('body',null,["class" => 'form-control']) }}
+				 
 			</div>
-		</div>
+
+			<div class="col-md-4">
+				<div class="card card-body bg-light">
+					<dl class="">
+					  <dt>Create At:</dt>
+					  <dd>{{ date('Y-m-j h:i a', strtotime($item->created_at)) }}</dd>
+					</dl>
+
+					<dl class="">
+					  <dt>Last Updated:</dt>
+					  <dd>{{ date('Y-m-j h:i a', strtotime($item->updated_at)) }}</dd>
+					</dl>
+					<hr>
+
+					<div class="row">
+						<div class="col-sm-6">
+							{!! Html::linkRoute('items.show', 'Cancel', array($item->id), array('class' => 'btn btn-danger btn-block'))!!}
+							{{-- <a href="#" class='btn btn-primary btn-block'>Edit</a> --}}
+						</div>
+						<div class="col-sm-6">
+							{{-- {!! Html::linkRoute('items.update', 'Save', array($item->id), array('class' => 'btn btn-success btn-block'))!!} --}}
+							{{ Form::submit('Save Changes', array('class' => 'btn btn-success btn-block'))}}				
+						</div>					
+					</div>				
+				</div>
+			</div>
+		</div><!-- end of .row -->
 		{!! Form::close() !!}
-	</div> <!-- end of .row -->
 @stop
